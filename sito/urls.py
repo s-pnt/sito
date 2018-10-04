c5740f81c990ab70b60748347f15c2984254b48f
s-pnt <stepnt@gmail.com>
2018-10-04 13:36:31 +0200

    Sito, 1st commit

---
+++ sito/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('python/introduzione/', views.python_introduzione, name='python_introduzione'),
+    path('python/installazione/', views.python_installazione, name='python_installazione'),
+    path('python/variabili/', views.python_variabili, name='python_variabili'),
+    path('python/stringhe/', views.python_stringhe, name='python_stringhe'),
+    path('python/liste/', views.python_liste, name='python_liste'),
+    path('linux/introduzione/', views.linux_introduzione, name='linux_introduzione'),
+    path('linux/distribuzioni/', views.linux_distribuzioni, name='linux_distribuzioni'),
+    path('raspberrypi/introduzione/', views.raspberrypi_introduzione, name='raspberrypi_introduzione'),
+]
---
+++ sito/views.py
@@ -0,0 +1,46 @@
+from django.shortcuts import render
+
+def home(request):
+    return render(request,
+                  'sito/home.html',
+                  {})
+
+def python_introduzione(request):
+    return render(request,
+                  'sito/python_introduzione.html',
+                  {})
+
+def python_installazione(request):
+    return render(request,
+                  'sito/python_installazione.html',
+                  {})
+
+def python_variabili(request):
+    return render(request,
+                  'sito/python_variabili.html',
+                  {})
+
+def python_stringhe(request):
+    return render(request,
+                  'sito/python_stringhe.html',
+                  {})
+
+def python_liste(request):
+    return render(request,
+                  'sito/python_liste.html',
+                  {})
+
+def linux_introduzione(request):
+    return render(request,
+                  'sito/linux_introduzione.html',
+                  {})
+
+def linux_distribuzioni(request):
+    return render(request,
+                  'sito/python_introduzione.html',
+                  {})
+
+def raspberrypi_introduzione(request):
+    return render(request,
+                  'sito/python_introduzione.html',
+                  {})
